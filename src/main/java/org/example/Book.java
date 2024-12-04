082c1588a3b2f451bc114c95071d9f9bba14dc2d
AlexLe06 <tanalexle06@gmail.com>
2024-12-03 20:19:26 -0500

    Modified isbn to a String data. Added toTitleCase method, but not working.

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -5,17 +5,16 @@ public class Book {
     private String author;
     private double price;
     private String publisher;
-    private long isbn;
+    private String isbn;
 
-    public Book(){
-
+    Book(){
     }
 
     public Book(String title){
         this.title = title;
     }
 
-    public Book(String title, String author, double price, String publisher, long isbn){
+    public Book(String title, String author, double price, String publisher, String isbn){
         this.title = title;
         this.author = author;
         this.price = price;
@@ -29,5 +28,32 @@ public class Book {
         price = newBook.price;
         publisher = newBook.publisher;
         isbn = newBook.isbn;
+    }
+
+    public int checkIsbnStatus(){
+        int isbn10;
+        int isbn13;
+        if (isbn == isbn10){
+            return 1;
+        }
+        else if (isbn == isbn13) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public void toTitleCase(){
+        char c = title.toUpperCase().charAt(0);
+        for (int i = 0; i < title.length(); i++){
+            if (title.charAt(i) == ' ')
+                title.toUpperCase().charAt(' ' + 1);
+            else
+                i++;
+
+            title = title;
+        }
+        System.out.println(title);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String ko = "baka hello";
+        Book kaka = new Book("hello baka", "alex", 20, "hitler", "f");
+        kaka.toTitleCase();
     }
 }
