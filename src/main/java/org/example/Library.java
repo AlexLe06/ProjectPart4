dea91cf4dfbd65be303a5db339da96f2b7dc2332
AlexLe06 <tanalexle06@gmail.com>
2024-12-05 12:11:47 -0500

    Added constructors for Library.java file and equal() in the Library.

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -5,5 +5,26 @@ import java.util.ArrayList;
  * @author Le Nguyen Tan Alex
  */
 public class Library {
-    ArrayList<String> book = new ArrayList<String>();
+    private ArrayList<Object> books;
+
+    public Library(){
+        this.books = new ArrayList<>(5);
+    }
+
+    public Library(ArrayList<Object> books){
+        this.books = new ArrayList<>(books);
+    }
+
+    public Object searchBook(String keyword){
+        ArrayList<Object> searchBook = new ArrayList<>(books);
+        for (int i = 0; i < searchBook.size(); i++){
+
+            if (books == keyword)
+        }
+        return 1;
+    }
+
+    public boolean equals(Library books){
+        return this.books.equals(books.books);
+    }
 }
